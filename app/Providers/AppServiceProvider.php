539c7b81d0b627c1b9e5cb6821bed0d1d29bb1a4
Ziri89 <davorvrzina89@gmail.com>
2020-12-27 23:26:04 +0100

    login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        $this->registerPolicies();
+
+        Passport::routes();
     }
 }
