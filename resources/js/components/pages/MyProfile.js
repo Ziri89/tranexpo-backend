7c1af317802f22867eac3e710ef5a9b349fa4681
razvigor <kamaradsasa@gmail.com>
2021-04-25 15:29:32 +0200

    my profile

--- resources/js/components/pages/MyProfile.js
+++ resources/js/components/pages/MyProfile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import Banner from "../header/Banner";
 import Storehous_1 from "../../img/storehous_1.jpg";
@@ -7,6 +8,14 @@ import "./MyProfile.css";
 
 const MyProfile = () => {
     const { t, i18n } = useTranslation();
+    const linkGenerator = link => {
+        // if the current language is the default language dont add the lang prefix
+        const languageLocale =
+            i18n.options.fallbackLng[0] === i18n.language
+                ? null
+                : i18n.language;
+        return languageLocale ? "/" + languageLocale + link : link;
+    };
     return (
         <div className="my-profile">
             <Banner
@@ -16,19 +25,35 @@ const MyProfile = () => {
             />
             <div className="container profile">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-12 mb-4">
                         <div className="card">
                             <div className="card-body">
                                 <h2 className="text-center">
                                     {t("hello")} Sasa Trkulja
                                 </h2>
-                                <h3></h3>
+                                <h3 className="text-center">
+                                    {t("see_profile")}
+                                </h3>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-md-6 col-lg-4">
                         <div className="card">
                             <div className="card-body">
+                                <dl>
+                                    <dt>{t("ship_from")}:</dt>
+                                    <dd>Bosnia and Hertzegovina, Sarajevo</dd>
+                                    <dt>{t("ship_to")}:</dt>
+                                    <dd>Switzerland, Zurich</dd>
+                                    <dt>{t("shipping_date")}</dt>
+                                    <dd>04/25/2021</dd>
+                                </dl>
+                                <Link
+                                    to={linkGenerator(`/single-post/some-id`)}
+                                    className="btn btn-danger"
+                                >
+                                    {t("see_more")}
+                                </Link>
                                 <ReactStars
                                     count={5}
                                     size={24}
@@ -39,15 +64,29 @@ const MyProfile = () => {
                                     }
                                     fullIcon={<i className="fa fa-star"></i>}
                                     activeColor="#ffd700"
-                                    value="4.5"
+                                    value={4.5}
                                     edit={false}
                                 />
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-md-6 col-lg-4">
                         <div className="card">
                             <div className="card-body">
+                                <dl>
+                                    <dt>{t("ship_from")}:</dt>
+                                    <dd>Bosnia and Hertzegovina, Sarajevo</dd>
+                                    <dt>{t("ship_to")}:</dt>
+                                    <dd>Switzerland, Zurich</dd>
+                                    <dt>{t("shipping_date")}</dt>
+                                    <dd>04/25/2021</dd>
+                                </dl>
+                                <Link
+                                    to={linkGenerator(`/single-post/some-id`)}
+                                    className="btn btn-danger"
+                                >
+                                    {t("see_more")}
+                                </Link>
                                 <ReactStars
                                     count={5}
                                     size={24}
@@ -58,15 +97,30 @@ const MyProfile = () => {
                                     }
                                     fullIcon={<i className="fa fa-star"></i>}
                                     activeColor="#ffd700"
-                                    value="4"
+                                    value={4}
                                     edit={false}
                                 />
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-md-6 col-lg-4">
                         <div className="card">
                             <div className="card-body">
+                                <dl>
+                                    <dt>{t("ship_from")}:</dt>
+                                    <dd>Bosnia and Hertzegovina, Sarajevo</dd>
+                                    <dt>{t("ship_to")}:</dt>
+                                    <dd>Switzerland, Zurich</dd>
+                                    <dt>{t("shipping_date")}</dt>
+                                    <dd>04/25/2021</dd>
+                                </dl>
+                                <Link
+                                    to={linkGenerator(`/single-post/some-id`)}
+                                    className="btn btn-danger"
+                                >
+                                    {t("see_more")}
+                                </Link>
+
                                 <ReactStars
                                     count={5}
                                     size={24}
@@ -77,7 +131,7 @@ const MyProfile = () => {
                                     }
                                     fullIcon={<i className="fa fa-star"></i>}
                                     activeColor="#ffd700"
-                                    value="5"
+                                    value={5}
                                     edit={false}
                                 />
                             </div>
