02deaba02d4f5acfe91e5a0f9631b6024bd34ab2
razvigor <kamaradsasa@gmail.com>
2021-06-28 22:21:44 +0200

    rating

--- resources/js/components/pages/Carrier.js
+++ resources/js/components/pages/Carrier.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
 import ReactStars from "react-rating-stars-component";
 import { useTranslation } from "react-i18next";
 import CarrierInformation from "../my-profile-card/CarrierInformation";
@@ -8,14 +9,49 @@ import Storehouse_2 from "../../img/storehous_2.jpg";
 import Banner from "../header/Banner";
 import Loader from "../../img/img-loader.gif";
 import "./Carrier.css";
+
 const Carrier = () => {
     const { t } = useTranslation();
+    const { user } = useSelector(state => state.auth);
     const [rate, setRate] = useState({
         stars: 0,
         comment: "",
-        shipper_id: ""
+        shipper_id: 1,
+        user_id: 1
     });
+    useEffect(() => {
+        console.log(rate);
+    }, [rate]);
+    const ratingSubmitHandler = ev => {
+        ev.preventDefault();
+        let formdata = new FormData();
+        formdata.append("stars", rate.stars);
+        formdata.append("comment", rate.comment);
+        formdata.append("shipper_id", rate.shipper_id);
+        formdata.append("user_id", rate.user_id);
 
+        axios
+            .post("api/rate", formdata, {
+                headers: {
+                    Authorization: `Bearer ${user.token ? user.token : null}`,
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => {
+                if (res) {
+                    setRate({
+                        stars: 0,
+                        comment: "",
+                        shipper_id: 1,
+                        user_id: user.id
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
     return (
         <div className="carrier">
             <Banner
@@ -40,7 +76,7 @@ const Carrier = () => {
 
                         <div className="col-12 col-lg-4">
                             <h2>{t("rate_the_carrier")}</h2>
-                            <form onSubmit={""}>
+                            <form onSubmit={ratingSubmitHandler}>
                                 <ReactStars
                                     count={5}
                                     size={24}
@@ -48,7 +84,7 @@ const Carrier = () => {
                                     emptyIcon={<i className="far fa-star"></i>}
                                     fullIcon={<i className="fa fa-star"></i>}
                                     activeColor="#ffd700"
-                                    value={rate.stars}
+                                    value={parseInt(rate.stars)}
                                     onChange={newRating => {
                                         setRate({
                                             ...rate,
@@ -77,6 +113,11 @@ const Carrier = () => {
                                         name="shipper_id"
                                         value={rate.shiper_id}
                                     />
+                                    <input
+                                        type="hidden"
+                                        name="user_id"
+                                        value={rate.shiper_id}
+                                    />
                                 </div>
                                 <button
                                     type="submit"
@@ -91,7 +132,7 @@ const Carrier = () => {
                             <h2>{t("ratings")}</h2>
                             <div className="row">
                                 <Rating
-                                    ratingVal="4.5"
+                                    ratingVal={5}
                                     comment="Lorem ipsum dolor sit amet consectetur,
                                         adipisicing elit. Dolores, quae dolor.
                                         Maiores praesentium quia qui voluptates
@@ -100,7 +141,7 @@ const Carrier = () => {
                                         iusto adipisci ad quisquam autem?"
                                 />
                                 <Rating
-                                    ratingVal="4"
+                                    ratingVal={4}
                                     comment="Lorem ipsum dolor sit amet consectetur,
                                         adipisicing elit. Dolores, quae dolor.
                                         Maiores praesentium quia qui voluptates
@@ -109,7 +150,7 @@ const Carrier = () => {
                                         iusto adipisci ad quisquam autem?"
                                 />
                                 <Rating
-                                    ratingVal="3"
+                                    ratingVal={3}
                                     comment="Lorem ipsum dolor sit amet consectetur,
                                         adipisicing elit. Dolores, quae dolor.
                                         Maiores praesentium quia qui voluptates
@@ -118,7 +159,7 @@ const Carrier = () => {
                                         iusto adipisci ad quisquam autem?"
                                 />
                                 <Rating
-                                    ratingVal="5"
+                                    ratingVal={5}
                                     comment="Lorem ipsum dolor sit amet consectetur,
                                         adipisicing elit. Dolores, quae dolor.
                                         Maiores praesentium quia qui voluptates
